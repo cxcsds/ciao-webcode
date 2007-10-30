d60986cbec0395a082297625070c211e70de4b36
Douglas Burke <dburke@cfa.harvard.edu>
2007-10-30 15:17:48 -0400

    Fix for publishing threads with 2 proglang versions (it works but there are still issues when using threadlink)

--- publish.pl
+++ publish.pl
@@ -1667,8 +1667,12 @@ EOX
 <p>Please choose the S-Lang or Python version of the thread
 "${thread_title_long}".</p>
 <list>
+<!--
  <li><threadlink name="${threadname}" proglang="sl">S-Lang</threadlink></li>
  <li><threadlink name="${threadname}" proglang="py">Python</threadlink></li>
+-->
+ <li><cxclink href="index.sl.html">S-Lang</cxclink></li>
+ <li><cxclink href="index.py.html">Python</cxclink></li>
 </list>
 </text>
 </page>
